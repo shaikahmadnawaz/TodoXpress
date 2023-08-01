2c40c189bb39f93604ea5324ae2182f5958741e3
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-08-01 11:13:30 +0530

    docker file created for mongo

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,26 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import mongoose, { Document, Schema, Model, model } from "mongoose";
+import mongoose, {
+  Document,
+  Schema,
+  Model,
+  model,
+  ConnectOptions,
+} from "mongoose";
 
 const app: Application = express();
 const port = 5000;
+
+const mongoURL = "mongodb://mongodb_container:27017/";
+
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello Bhai!");
 });
 
 app.listen(port, () => {
